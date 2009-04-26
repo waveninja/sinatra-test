2d6a97c1dfdacf4127fe11741a077a5003ca994f
waveninja <waveninja@gmail.com>
2009-04-26 18:49:06 +0200

    add helpers

--- shortener.rb
+++ shortener.rb
@@ -8,6 +8,20 @@ before do
   @shorten = Shortener.load
 end
 
+helpers do
+
+  def base_url
+    base = "http://#{Sinatra::Application.host}"
+    port = Sinatra:Application.port == 80 ? base : base <<
+":#{Sinatra::Application.port}"
+  end
+
+  def url(path='')
+    [base_url, path].join('/')
+  end
+
+end
+
 get '/' do
   erb :home
 end
